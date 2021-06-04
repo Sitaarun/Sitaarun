221c87ca94f0d871e11b9006be8d132b36abeed0
Sitaarun <intellitest29@gmail.com>
2021-06-04 17:06:25 -0400

    Modified by by the Employee1

---
+++ mmp/src/main/java/org/iit/healthcare/mmp/patientmodule/pages/HelloWorl.java
@@ -0,0 +1,9 @@
+package org.iit.healthcare.mmp.patientmodule.pages;
+
+public class HelloWorl {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World, Welcome to the Automation World");
+	}
+
+}
